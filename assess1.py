ff6eb822ceec974cb7b7d3a6fdc8350a1dc78f8a
Marcus Klingborg <marcus.klingborg@live.jp>
2022-12-06 11:02:21 +0100

    Delete function Done

--- assess1.py
+++ assess1.py
@@ -32,7 +32,29 @@ def connect_db():
 
 
 def list_data(conn):
-    print("Do something")
+    try:
+        # Create a cursor.
+        cur = conn.cursor()
+
+        # Executing a statement.
+        cur.execute("SELECT * FROM view_contacts;")
+
+        # Check return from executed statement.
+        rows = cur.fetchall()
+
+        if rows is None:
+            print("It seems as if there are no saved contacts.")
+        else:
+            print("Number of entries: " + str(cur.rowcount))
+            for row in rows:
+                print(f"{row[0]} {row[1]}, {row[2]}, {row[3]}, {row[4]}")
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        # Close communication with databäase.
+        cur.close()
 
 
 # TODO 4. Insert function
@@ -40,9 +62,38 @@ def insert_data(conn):
     print("Do something")
 
 
-# TODO 5. Delete function
-def delete_data(conn):
-    print("Do something")
+def delete_data(conn, first_name, last_name):
+    try:
+        # Create a cursor:
+        cur = conn.cursor()
+
+        # Check if name in phonelist
+
+        # Executing a statement.
+        cur.execute("SELECT * FROM view_contacts;")
+
+        # Check return from executed statement.
+        rows = cur.fetchall()
+
+        for row in rows:
+            if first_name == row[0] and last_name == row[1]:
+                # Executing statement.
+                cur.execute(f"""DELETE FROM contacts
+WHERE first_name = '{first_name}'
+AND last_name = '{last_name}';""")
+            print(f"{first_name} {last_name} deleted.")
+        else:
+            print("Name not recognized.")
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        # Save table.
+        save_changes(conn)
+
+        # Close communication with database.
+        cur.close()
 
 
 def save_changes(conn):
@@ -57,27 +108,37 @@ def save_changes(conn):
 # Running the program
 conn = connect_db()
 
+# Messages
+print("Please enter one of the following commands:")
+print("")
+print("LIST - Prints a list of all contacts.")
+print("INSERT - Inserts contact to list.")
+print("DELETE first_name last_name - Deletes contact from list..")
+print("QUIT - Exists the program.")
+
 while True:
     user_input = input("Command: ").strip().split()
 
     user_input[0] = user_input[0].upper()
 
     # Formatting name
-    name = " ".join(user_input[1:3])
-    name = name.title()
+    first_name = user_input[1]
+    first_name = name.title()
+    last_name = user_input[2]
+    last_name = name.title()
 
     # Selection menu
     # LIST
     if user_input[0] == "LIST":
         list_data(conn)
 
-    # ADD
+    # INSERT
     elif user_input[0] == "INSERT":
         insert_data(conn)
 
     # DELETE
     elif user_input[0] == "DELETE":
-        delete_data(conn)
+        delete_data(conn, first_name, last_name)
 
     # QUIT
     elif user_input[0] == "QUIT":
