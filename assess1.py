d98dac946db4b00db779df605e3cdc08a19f98ca
Marcus Klingborg <marcus.klingborg@live.jp>
2022-12-06 10:28:07 +0100

    Outline created

--- assess1.py
+++ assess1.py
@@ -1,1 +1,21 @@
 #! python3
+
+# Creating project outline
+
+# TODO 1. Connect to database
+# TODO Commit
+
+# TODO 2. Fetch data
+# TODO 2.1 Print to console
+# TODO Commit
+
+# TODO 3. Control-loop
+# TODO 3.1 Commands
+# TODO 3.1.1 LIST
+# TODO 3.1.2 INSERT
+# TODO 3.1.3 DELETE
+# TODO Commit
+
+# TODO 4. Insert function
+
+# TODO 5. Delete function
